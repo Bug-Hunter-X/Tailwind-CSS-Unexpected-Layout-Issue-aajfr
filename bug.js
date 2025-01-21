591fbd2138d42a61c8430f5a8f97fe1eee56eec7
BugHunter-X <askzenai@gmail.com>
2025-01-21 07:14:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,7 @@
+```javascript
+// ... your Tailwind CSS classes
+<div class="bg-gray-100 p-4">
+  <h2 class="text-2xl font-bold">My Component</h2>
+  <p class="text-gray-700">This component has a strange layout issue.</p>
+</div>
+```
